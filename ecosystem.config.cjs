036153b00fedd451e1fd60a94fce2e1520e9c7b5
thisloadme <dionbudi7@gmail.com>
2025-06-08 11:11:52 +0700

    Update server port configuration in ecosystem.config.cjs and nuxt.config.ts from 8000 to 8080

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'dota-draft-simulator',
-      port: '8000',
+      port: '8080',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
